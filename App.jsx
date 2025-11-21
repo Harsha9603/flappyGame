a85e46d1a923d255dbd3e5abbbb723db273c7a75
Harsha9603 <hnallella@algomau.ca>
2025-11-20 22:22:09 -0500

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -5,9 +5,9 @@ import Game from "./Game.jsx";
 function App() {
   return (
     <div className="app">
-      <h1 className="title">Flappy Box</h1>
+      <h1 className="title">FlappyGame</h1>
       <Game />
-      <p className="hint">Click or press SPACE to jump</p>
+      <p className="hint"> Press SPACE to jump</p>
     </div>
   );
 }
